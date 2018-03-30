06fe47c19dfcb5b5f2ab380f5321a7a331ddfb41
Paweł Strzałkowski <pawel@praesens.co>
2018-03-30 14:47:30 +0200

    Allow show for normal products

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -4,6 +4,7 @@ Spree::ProductsController.class_eval do
   private
 
   def prevent_reward_show
-    redirect_to root_path
+    product = @product || Spree::Product.find(params[:id])
+    redirect_to root_path if Spree::AnnexCloudReward.where(variant: product.variants_including_master).any?
   end
 end
