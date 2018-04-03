aeb59d28528ecad2f0d5b90af1452e0b5fb07c4a
Paweł Strzałkowski <pawel@praesens.co>
2018-04-03 14:29:58 +0200

    Sales tracking pixel

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -6,4 +6,13 @@ Spree::LineItem.class_eval do
   def annex_cloud_points_required
     variant.annex_cloud_points_required * quantity
   end
+
+  def annex_cloud_data
+    variant.
+      annex_cloud_data.
+      merge(
+        price: price,
+        qty: quantity
+      )
+  end
 end
--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -5,8 +5,53 @@ Spree::Order.class_eval do
     line_items.sum(&:annex_cloud_points_required)
   end
 
+  def annex_cloud_pixel_params
+    {
+      site_id: SpreeAnnexCloud.configuration.site_id,
+      order_id: number,
+      sale_amount: total,
+      email_id: email,
+      name: user.try(:full_name),
+      fname: user.try(:firstname),
+      lname: user.try(:lastname),
+      order_discount: adjustments.promotion.sum(&:amount),
+      coupon: promo_code,
+      rewards_applied: annex_cloud_reward_json,
+      exclude_products: annex_cloud_product_json
+    }.map { |key, value| "#{key}=#{value}" }.join('&')
+  end
+
   def annex_cloud_reward?
     line_items.any?(&:annex_cloud_reward?)
+  end
+
+  def annex_cloud_rewards
+    rewards = {}
+    line_items.
+      select(&:annex_cloud_reward?).each do |line|
+        variant = line.variant
+        reward_id = variant.annex_cloud_reward.id
+        rewards[reward_id] ||= { rid: reward_id, quantity: 0, sku: variant.sku }
+        rewards[reward_id][:quantity] += line.quantity
+      end
+    rewards.values.each_with_index.map { |reward, index| [index, reward] }.to_h
+  end
+
+  def annex_cloud_reward_json
+    annex_cloud_rewards.to_json
+  end
+
+  def annex_cloud_products
+    line_items.
+      reject(&:annex_cloud_reward?).
+      map(&:annex_cloud_data).
+      each_with_index.map do |line, index|
+        [index, line]
+      end.to_h
+  end
+
+  def annex_cloud_product_json
+    annex_cloud_products.to_json
   end
 
   def annex_cloud_reward_valid?
