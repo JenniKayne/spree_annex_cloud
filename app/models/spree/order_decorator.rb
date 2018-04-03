be93acf3ca60f33e5069f02fb68d37a80678e865
Paweł Strzałkowski <pawel@praesens.co>
2018-04-03 14:32:39 +0200

    Methods in proper order

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,10 +1,6 @@
 Spree::Order.class_eval do
   register_line_item_comparison_hook :line_item_reject_match_for_annex_cloud_rewards
 
-  def annex_cloud_points_required
-    line_items.sum(&:annex_cloud_points_required)
-  end
-
   def annex_cloud_pixel_params
     {
       site_id: SpreeAnnexCloud.configuration.site_id,
@@ -21,24 +17,8 @@ Spree::Order.class_eval do
     }.map { |key, value| "#{key}=#{value}" }.join('&')
   end
 
-  def annex_cloud_reward?
-    line_items.any?(&:annex_cloud_reward?)
-  end
-
-  def annex_cloud_rewards
-    rewards = {}
-    line_items.
-      select(&:annex_cloud_reward?).each do |line|
-        variant = line.variant
-        reward_id = variant.annex_cloud_reward.id
-        rewards[reward_id] ||= { rid: reward_id, quantity: 0, sku: variant.sku }
-        rewards[reward_id][:quantity] += line.quantity
-      end
-    rewards.values.each_with_index.map { |reward, index| [index, reward] }.to_h
-  end
-
-  def annex_cloud_reward_json
-    annex_cloud_rewards.to_json
+  def annex_cloud_points_required
+    line_items.sum(&:annex_cloud_points_required)
   end
 
   def annex_cloud_products
@@ -54,8 +34,28 @@ Spree::Order.class_eval do
     annex_cloud_products.to_json
   end
 
+  def annex_cloud_reward?
+    line_items.any?(&:annex_cloud_reward?)
+  end
+
+  def annex_cloud_reward_json
+    annex_cloud_rewards.to_json
+  end
+
   def annex_cloud_reward_valid?
     user.nil? || annex_cloud_points_required <= user.annex_cloud_available_points
+  end
+
+  def annex_cloud_rewards
+    rewards = {}
+    line_items.
+      select(&:annex_cloud_reward?).each do |line|
+        variant = line.variant
+        reward_id = variant.annex_cloud_reward.id
+        rewards[reward_id] ||= { rid: reward_id, quantity: 0, sku: variant.sku }
+        rewards[reward_id][:quantity] += line.quantity
+      end
+    rewards.values.each_with_index.map { |reward, index| [index, reward] }.to_h
   end
 
   def ensure_annex_cloud_reward_valid
