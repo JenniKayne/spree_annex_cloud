218921981312c9ecaa32290c46d6b63c151873ed
Michael Romano <micha7a.com@gmail.com>
2019-07-17 14:40:20 -0400

    Revert "Feature/allow for gift card creation without order"

--- app/services/spree_annex_cloud/reward_service.rb
+++ app/services/spree_annex_cloud/reward_service.rb
@@ -9,8 +9,7 @@ module SpreeAnnexCloud
     end
 
     def call
-      create_perform if create_allowed? && !reward.product.is_gift_card?
-
+      create_perform if create_allowed?
       if @error
         return json_error(@error)
       else
@@ -31,7 +30,7 @@ module SpreeAnnexCloud
     end
 
     def can_redeem_free_reward?
-      if reward.points_required.zero? && item_already_redeemed?
+      if reward.points_required.zero? && has_this_free_product?
         @error = Spree.t('annex_cloud.already_redeem_free_product')
         return false
       end
@@ -44,7 +43,7 @@ module SpreeAnnexCloud
       enough_points? && can_redeem_free_reward?
     end
 
-    def item_already_redeemed?
+    def has_this_free_product?
       order.user.orders.joins(:line_items).
         where('spree_line_items.variant_id = ? AND spree_line_items.price = ?', reward.variant_id, 0.to_f).
         count.positive?
